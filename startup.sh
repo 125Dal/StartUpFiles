ea6744d67db333f6f3a31423439bd550cf1d8324
125Dal <12.05.dal.1999@gmail.com>
2021-03-10 03:26:58 +0900

    :memo: Add 2021.03.10

---
+++ startup.sh
@@ -0,0 +1,25 @@
+name='SetupDirectory'
+
+cd ~
+mkdir $name
+cd $name
+
+git clone git@github.com:125Dal/Brewfile.git
+git clone git@github.com:125Dal/.zshrc.git
+git clone git@github.com:125Dal/Hyper.js.git
+
+brew bundle
+
+mv .zshrc/.zshrc ~
+mv Hyper.js/.hyper.js ~
+
+echo export PATH="/usr/local/bin/git/:$PATH:/opt/homebrew/bin/" >> ~/.zshrc
+echo typeset -U PATH >> ~/.zshrc
+
+cd ..
+rm -Rf $name
+
+thisname='StartUpFiles'
+startupfiles=$(find ~/Documents -type d -name $thisname)
+[[ $startupfiles != $name ]] && cd $startupfiles
+rm -Rf $thisname
